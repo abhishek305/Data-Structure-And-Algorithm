4ae9dcd77b05e8b3efc1ea8500afafed6f3992d7
Abhishek Ezhava <Abhishekshaji32@gmail.com>
2019-03-18 21:28:09 +0530

    Add files via upload

---
+++ Prime Using TC.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int tc;
+	cin>>tc;
+	for(int i=0;i<tc;i++)
+	{
+		int num;
+		cin>>num;
+		for(int j=2;j<=2;j++)
+		{
+		 if (num%j==0)	{
+		 	cout<<"No";
+		 	break;
+		 }
+		 else
+		 {
+		 	cout<<"Yes";
+			 }	
+		 }
+		}
+	}
+
